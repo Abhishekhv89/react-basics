17126e0edcb312e1a30455e24e8c73293967c4c6
abhishek <oneminddevelopers@gmail.com>
2025-03-13 09:04:23 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "This is a Heading - React"
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "This is a Heading - React"
+// );
+
+const DisplaySomething = () => {
+  return (
+    <>
+      <h1>This is another heading - React Components</h1>
+    </>
+  );
+};
+
+const heading = (
+  <>
+    {DisplaySomething()}
+    <h1>This is a Heading - from JSX</h1>
+  </>
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
